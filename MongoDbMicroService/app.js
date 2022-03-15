6482a10eddb6fc22f637bf11a9e7717bab958b30
josh6512 <97390799+josh6512@users.noreply.github.com>
2022-03-15 15:21:04 +0200

    comit

--- MongoDbMicroService/app.js
+++ MongoDbMicroService/app.js
@@ -1,29 +1,22 @@
-const express = require('express');
-const cors = require('cors');
-const container = require('./configContainer');
-const userRep  = require('./repo/userRepo')
-const config = container.resolve('config');
+const express = require("express");
+const cors = require("cors");
+const container = require("./configContainer");
+const userRep = require("./repo/userRepo");
+const config = container.resolve("config");
+const userRouter = require("./Routes/userRouter");
 
-const PORT = config.get('serverPort.port');
-const origin  = config.get('serverPort.originAllowed');
+const PORT = config.get("serverPort.port");
+const origin = config.get("serverPort.originAllowed");
 const app = express();
 
-
-
 app.use(express.json());
-app.use(cors ({
-    origin: origin
-}));
+app.use(
+  cors({
+    origin: origin,
+  })
+);
+app.use("/user", userRouter);
 
-app.listen(PORT, ()=>{
-    console.log(`server is running on PORT : 3000`);
+app.listen(PORT, () => {
+  console.log(`server is running on PORT : 3000`);
 });
-
-app.post('/add',async (req,res)=>{
-    let userrep = new userRep();
-    console.log(req.body);
-    let result = await userrep.addUserRep(req.body);
-    console.log(result);
-    res.send(result)
-
-})
