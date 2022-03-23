f9b54f0b892e3480fe03a3256c3499b8f95eb164
josh6512 <97390799+josh6512@users.noreply.github.com>
2022-03-23 20:43:50 +0200

    comit

--- Clinet/fakelook/src/Views/LoginPage/Componnets/googleLogin.js
+++ Clinet/fakelook/src/Views/LoginPage/Componnets/googleLogin.js
@@ -1,71 +1,66 @@
+import GoogleLogin from "react-google-login";
+import { useState } from "react";
+import React from "react";
+const GOOGLE_CLIENT_ID =
+  "378037402068-hnpf4gt6obl7o2nvdqf3fmfoc55u34qv.apps.googleusercontent.com";
+const GOOGLE_CLIENT_SECRET = "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";
 
-import GoogleLogin from 'react-google-login';
-import { useState } from 'react';
-import React from 'react';
-const GOOGLE_CLIENT_ID = 
-"378037402068-hnpf4gt6obl7o2nvdqf3fmfoc55u34qv.apps.googleusercontent.com"
-const GOOGLE_CLIENT_SECRET="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"
+const GoogleLoginComp = (props) => {
+  const [loginData, setLoginData] = useState(
+    localStorage.getItem("loginData")
+      ? JSON.parse(localStorage.getItem("loginData"))
+      : null
+  );
 
+  const handleFailure = (result) => {
+    alert(result.tokenId);
+  };
 
-const GoogleLoginComp = (props)=>{
-
-    
-    const [loginData, setLoginData] = useState(
-        localStorage.getItem('loginData')
-          ? JSON.parse(localStorage.getItem('loginData'))
-          : null
-      );
-
-      const handleFailure = (result) => {
-        alert(result.tokenId);
-      };
-    
-      const handleLogin = async (temp) => {
-          console.log(temp.tokenId);
-        const res = await fetch('http://localhost:5001/gogRouter/api/google-login', {
-          method: 'POST',
-          body: JSON.stringify({
-            token: temp.tokenId,
-          }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        });
+  const handleLogin = async (temp) => {
+    console.log(temp.tokenId);
+    const res = await fetch(
+      "http://localhost:5001/gogRouter/api/google-login",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          token: temp.tokenId,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
     console.log(res);
-        const data = await res.json();
-        setLoginData(data);
-        console.log(data.email);
-        localStorage.setItem('loginData', JSON.stringify(data));
-      };
+    const data = await res.json();
+    setLoginData(data);
+    console.log(data.email);
+    localStorage.setItem("loginData", JSON.stringify(data));
+  };
 
-      const handleLogout = () => {
-        localStorage.removeItem('loginData');
-        setLoginData(null);
-      };
-    
+  const handleLogout = () => {
+    localStorage.removeItem("loginData");
+    setLoginData(null);
+  };
 
-    return(
-        <div>
-
-        <h1>React Google Login App</h1>
-        <div>
-          {loginData ? (
-            <div>
-              <h3>You logged in as {loginData.email}</h3>
-              <button onClick={handleLogout}>Logout</button>
-            </div>
-          ) : (
-            <GoogleLogin
-              clientId={GOOGLE_CLIENT_ID}
-              buttonText="Log in with Google"
-              onSuccess={handleLogin}
-             
-              cookiePolicy={'single_host_origin'}
-            ></GoogleLogin>
-          )}
-        </div>
-
-        </div>
-        )
+  return (
+    <div>
+      <h1>React Google Login App</h1>
+      <div>
+        {loginData ? (
+          <div>
+            <h3>You logged in as {loginData.email}</h3>
+            <button onClick={handleLogout}>Logout</button>
+          </div>
+        ) : (
+          <GoogleLogin
+            clientId={GOOGLE_CLIENT_ID}
+            buttonText="Log in with Google"
+            onSuccess={handleLogin}
+            cookiePolicy={"single_host_origin"}
+          ></GoogleLogin>
+        )}
+      </div>
+    </div>
+  );
 };
 export default GoogleLoginComp;
