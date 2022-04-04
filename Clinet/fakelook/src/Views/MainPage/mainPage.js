852fa45423c3bd1b989d64b5d92993ca21d4b927
tomer <tomernfood@gmail.com>
2022-04-04 21:57:08 +0300

    comit

--- Clinet/fakelook/src/Views/MainPage/mainPage.js
+++ Clinet/fakelook/src/Views/MainPage/mainPage.js
@@ -80,11 +80,11 @@ function MainPage() {
             <label>image tags</label>
             <input type="text" onChange={(e) => SetImageTags(e.target.value)} />
             <br />
-            <label>tagget users</label>
+            {/* <label>tagget users</label>
             <input
               type="text"
               onChange={(e) => SetTaggetUsers(e.target.value)}
-            />
+            /> */}
           </form>
           <button onClick={onSearch()}></button>
 
@@ -106,11 +106,12 @@ function MainPage() {
             
           </div>
         </div>
-        <div className="child2">
+        <div className="">
          
-          <SimpleMap></SimpleMap>
-        </div>
+         <SimpleMap></SimpleMap>
+       </div>
       </div>
+      
     </div>
   );
 }
